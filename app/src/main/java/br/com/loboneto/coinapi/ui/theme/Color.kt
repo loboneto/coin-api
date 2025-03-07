5fe540140fb780b91c0e6f7b978508b1004bc202
lobo-labs <loboneto@lobolabs.com.br>
2025-03-07 13:47:19 -0300

    ui update

--- app/src/main/java/br/com/loboneto/coinapi/ui/theme/Color.kt
+++ app/src/main/java/br/com/loboneto/coinapi/ui/theme/Color.kt
@@ -2,9 +2,11 @@ package br.com.loboneto.coinapi.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 val BlackLight = Color(0xFF15181a)
 val GrayLight = Color(0xFFedf2f3)
 val Gray = Color(0xFFaaacad)
 val Orange = Color(0xFFff5000)
+val OrangeLight = Color(0xFFffeae5)
 val OrangeDark = Color(0xFF67281d)
